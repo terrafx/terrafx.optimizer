5c2b279213a97bdbe6aa9d449c2c7ff8b5e96f67
Tanner Gooding <tagoo@outlook.com>
2019-07-03 13:47:31 -0700

    Adding a TerraFX.Optimization project

---
+++ sources/Optimizer/Program.cs
@@ -0,0 +1,29 @@
+// Copyright Â© Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace TerraFX.Optimization
+{
+    public static class Program
+    {
+        private static RootCommand s_rootCommand;
+
+        public static async Task<int> Main(params string[] args)
+        {
+            s_rootCommand = new RootCommand();
+            {
+                s_rootCommand.Description = "TerraFX IL Optimizer";
+                s_rootCommand.Handler = CommandHandler.Create(typeof(Program).GetMethod(nameof(Run)));
+            }
+            return await s_rootCommand.InvokeAsync(args);
+        }
+
+        public static int Run(InvocationContext context)
+        {
+            int exitCode = 0;
+            return exitCode;
+        }
+    }
+}
